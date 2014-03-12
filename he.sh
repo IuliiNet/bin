4c0f0ae8f460146448d6640927567a68728de3d3
Luca Postregna <luca.postregna@gmail.com>
2014-03-12 20:02:30 +0100

    Update he.sh

--- he.sh
+++ he.sh
@@ -1,6 +1,8 @@
 #!/bin/sh 
 
 IPV4=`ip a s ppp0 |grep inet |awk '{print $2}'`
+USER="XXX"
+PWD="YYY"
 
 ip t d he-ipv6
 ip tunnel add he-ipv6 mode sit remote 216.66.80.26 local $IPV4 ttl 255
@@ -9,4 +11,4 @@ ip addr add 2001:470:1f08:79e::2/64 dev he-ipv6
 ip route add ::/0 dev he-ipv6
 ip -f inet6 addr
 
-wget -O /dev/null https://lucapost:N4t150n3@ipv4.tunnelbroker.net/ipv4_end.php?tid=190038 >> /dev/null 2>&1
+wget -O /dev/null https://$USER:$PWD@ipv4.tunnelbroker.net/ipv4_end.php?tid=190038 >> /dev/null 2>&1
